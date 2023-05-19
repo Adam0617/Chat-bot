1c4ff754052b94b4a9c67da1814ec2fa843c7c34
Julia <81326278+d-della@users.noreply.github.com>
2023-05-19 08:21:47 +0200

    Add files via upload

---
+++ qa-library.js
@@ -0,0 +1,31 @@
+var questionAnswerLibrary = {
+    greetings: {
+      questions: ['hello', 'hi', 'hey'],
+      answers: ['Anonym: Hello!', 'Anonym: Hi there!', 'Anonym: Greetings!']
+    },
+    howAreYou: {
+      questions: ['how are you', 'how are u'],
+      answers: [
+        'Anonym: I am doing great, thank you!',
+        'Anonym: Feeling good!',
+        'Anonym: Pretty well, how about you?'
+      ]
+    },
+    origin: {
+      questions: ['where are you from', 'where do you come from', 'where are u from'],
+      answers: [
+        'Anonym: I am from Czech Republic.',
+        'Anonym: I come from London.',
+        'Anonym: That is none of your business.'
+      ]
+    },
+    name: {
+        questions: ['what is your name', 'whats your name', 'what do they call you'],
+        answers: [
+          'Anonym: Saša',
+          'Anonym: Anna',
+          'Anonym: Petr'
+        ]
+    }
+  };
+  

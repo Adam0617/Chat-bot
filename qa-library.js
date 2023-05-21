d0375111af9b1db93c3aae264abb63a3d9e107f9
Julia <81326278+d-della@users.noreply.github.com>
2023-05-21 13:24:37 +0200

    Update qa-library.js

--- qa-library.js
+++ qa-library.js
@@ -4,13 +4,14 @@ var questionAnswerLibrary = {
       answers: ['Anonym: Ahoj!', 'Anonym: čus', 'Anonym: dobrý den','Anonym:čau','Anonym:nazdar',]
     },
     howAreYou: {
-      questions: ['jak se mas ?', 'jak se vede ?','Jak je ?','jak to šlape','Jak to jde?','Jak se máte','Jak se daří','Co se děje','Jak to šlo','jak bylo','jak ses měl',],
+      questions: ['jak se mas', 'jak se vede','Jak je','jak to šlape','Jak to jde','Jak se máte','Jak se daří','Co se děje','Jak to šlo','jak bylo','jak ses měl',],
       answers: [
-        'Anonym: Mám se dobře akorát mě nic nebaví co ty ?',
+        'Anonym: Mám se dobře akorát mě nic nebaví co ty?',
         'Anonym: Mám se výborně',
         'Anonym: Pretty well, how about you?'
       ]
     },
+    
     origin: {
       questions: ['where are you from', 'where do you come from', 'where are u from'],
       answers: [
@@ -19,6 +20,7 @@ var questionAnswerLibrary = {
         'Anonym: That is none of your business.'
       ]
     },
+
     name: {
         questions: ['what is your name', 'whats your name', 'what do they call you'],
         answers: [
@@ -26,6 +28,42 @@ var questionAnswerLibrary = {
           'Anonym: Anna',
           'Anonym: Petr'
         ]
+    },
+
+    age: {
+      questions: ['how old are you', 'whats your age', 'what is your age'],
+      answers: [
+        'Anonym: I am ageless.',
+        'Anonym: Age is just a number for me.',
+        'Anonym: I exist beyond the concept of age.'
+      ]
+    },
+
+    hobbies: {
+      questions: ['what are your hobbies', 'do you have any hobbies', 'what do you like to do'],
+      answers: [
+        'Anonym: I enjoy conversing with humans.',
+        'Anonym: I spend my time learning and gathering knowledge.',
+        'Anonym: I don\'t have physical hobbies, but I am interested in various topics.'
+      ]
+    },
+
+    favoriteColor: {
+      questions: ['what is your favorite color', 'whats your favorite color', 'which color do you like'],
+      answers: [
+        'Anonym: I don\'t have the ability to perceive or appreciate colors.',
+        'Anonym: Colors hold no significance for me.',
+        'Anonym: I am indifferent to colors.'
+      ]
+    },
+
+    foodPreference: {
+      questions: ['what is your favorite food', 'whats your favorite food', 'what do you like to eat'],
+      answers: [
+        'Anonym: As an AI, I do not have the ability to eat or taste food.',
+        'Anonym: I don\'t have preferences for food as I don\'t require sustenance.',
+        'Anonym: Food is not a part of my existence.'
+      ]
     }
   };
   
